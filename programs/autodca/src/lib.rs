21b3ae25e7d94d1c0c625ab0afe18dfcff6a4715
RohanXKapur <rohankapurdev@outlook.com>
2022-05-31 00:36:55 +0800

    add scaffolding

--- programs/autodca/src/lib.rs
+++ programs/autodca/src/lib.rs
@@ -6,12 +6,58 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod autodca {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeCrankAuthority>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
+pub struct InitializeCrankAuthority<'info> {
     pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct TransferCrankAuthority<'info> {
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct AcceptCrankAuthority<'info> {
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct SetCrankTreasury<'info> {
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct InitializeDcaMetadata<'info> {
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct TriggerDcaPayment<'info> {
+    pub signer: Signer<'info>
+}
+
+#[account]
+pub struct CrankAuthority {
+    pub current_authority: Pubkey,
+    pub pending_authority: Pubkey,
+    pub crank_treasury: Pubkey
+}
+
+#[account]
+pub struct DcaMetadata {
+    pub owner: Pubkey,
+    pub from_token_mint: Pubkey,
+    pub to_token_mint: Pubkey,
+    pub owner_from_token_account: Pubkey,
+    pub owner_to_token_account: Pubkey,
+    pub amount_delegated: u64,
+    pub interval: i64,
+    pub crank_authority: Pubkey,
+    pub invocations: u16,
+    pub created_at: i64,
 }
