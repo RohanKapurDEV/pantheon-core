bf5023f3c3e14fb40db3362b9ea4287b336845c6
RohanXKapur <rohankapurdev@outlook.com>
2022-06-08 05:02:30 +0800

    temp commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pantheon Core
+
+Pantheon is a Solana protocol for vaults that automate trading flows onchain.
+
+## Programs
+
+- `autodca` - Automated dollar cost averaging vaults, powered by [Jupiter Exchange](https://www.jup.ag) for best-in-class price execution
+
+## Building and running tests
--- programs/autodca/src/instructions/withdraw_token_from_metadata.rs
+++ programs/autodca/src/instructions/withdraw_token_from_metadata.rs
@@ -1,10 +1,25 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::TokenAccount;
+
+use crate::state::{CrankAuthority, DcaMetadata};
 
 #[derive(Accounts)]
 pub struct WithdrawTokenFromMetadata<'info> {
     pub payer: Signer<'info>,
+
+    pub dca_metadata: Account<'info, DcaMetadata>,
+
+    pub crank_authority: Account<'info, CrankAuthority>,
+
+    pub from_mint_user_token_account: Account<'info, TokenAccount>,
+
+    pub to_mint_user_token_account: Account<'info, TokenAccount>,
+
+    pub from_mint_vault_token_account: Account<'info, TokenAccount>,
+
+    pub to_mint_vault_token_account: Account<'info, TokenAccount>,
 }
 
-pub fn handler(ctx: Context<WithdrawTokenFromMetadata>) -> Result<()> {
+pub fn handler(ctx: Context<WithdrawTokenFromMetadata>, from_token: bool) -> Result<()> {
     Ok(())
 }
