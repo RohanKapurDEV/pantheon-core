abf0ff876ae7f59fea0b35c4ea2a3d05a3bf4e46
RohanXKapur <rohankapurdev@outlook.com>
2022-06-08 22:48:43 +0800

    temp commit

--- programs/autodca/src/lib.rs
+++ programs/autodca/src/lib.rs
@@ -65,15 +65,6 @@ pub mod autodca {
     }
 }
 
-/// This instruction is effectively a withdrawal to the current_authority's token account so they can
-/// run the funds through jupiter aggregator. Unfortunately, the jupiter swap cannot be called from an
-/// onchain program and so funds must be temporally extracted from the contract and run through typescript
-/// code that calls the jupiter sdk sensibly
-#[derive(Accounts)]
-pub struct TriggerDcaPayment<'info> {
-    pub payer: Signer<'info>,
-}
-
 #[derive(Accounts)]
 pub struct CloseDcaMetadata<'info> {
     pub payer: Signer<'info>,
