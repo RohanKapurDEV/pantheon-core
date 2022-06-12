e161b5679637d488315f8db940c47f4f1d91091d
RohanXKapur <rohankapurdev@outlook.com>
2022-06-12 21:36:57 +0800

    tests base

---
+++ tests/utils.ts
@@ -0,0 +1,21 @@
+import BN from "bn.js";
+
+// .to_le_bytes() for u64
+export const bnTo8 = (bn: BN): Uint8Array => {
+  return Buffer.from([...bn.toArray("le", 8)]);
+};
+
+// .to_le_bytes() for u16
+export const bnTo16 = (bn: BN): Uint8Array => {
+  return Buffer.from([...bn.toArray("le", 2)]);
+};
+
+// .to_le_bytes() for u8
+export const bnTo1 = (bn: BN): Uint8Array => {
+  return Buffer.from([...bn.toArray("le", 1)]);
+};
+
+// sleeper function
+export const delay = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
