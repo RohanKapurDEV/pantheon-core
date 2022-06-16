ed34f941d76db3cd2538369f3a056e723e5266a6
RohanXKapur <rohankapurdev@outlook.com>
2022-06-16 09:08:30 +0800

    ix base

---
+++ offchain/autodca-cli/src/instructions/initialize_crank_authority.rs
@@ -0,0 +1,8 @@
+use anchor_client::ClientError;
+use autodca::accounts as autodca_accounts;
+use autodca::instruction as autodca_ixs;
+use autodca::state::*;
+
+pub async fn initialize_crank_authority() -> Result<bool, ClientError> {
+    todo!()
+}
---
+++ offchain/autodca-cli/src/instructions/mod.rs
@@ -0,0 +1,3 @@
+pub mod initialize_crank_authority;
+
+pub use initialize_crank_authority::*;
