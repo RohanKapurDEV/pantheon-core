79f27d28aed722cc0de9feae72034df798fa38fc
RohanXKapur <rohankapurdev@outlook.com>
2022-06-26 22:03:27 +0800

    add instruction to module

--- local/autodca-cli/src/instructions/mod.rs
+++ local/autodca-cli/src/instructions/mod.rs
@@ -1,5 +1,5 @@
 pub mod initialize_crank_authority;
-pub mod initialize_dca_authority;
+pub mod initialize_dca_metadata;
 
 pub use initialize_crank_authority::*;
-pub use initialize_dca_authority::*;
+pub use initialize_dca_metadata::*;
