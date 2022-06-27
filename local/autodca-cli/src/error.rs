bbe7d044ea4b92a29e954115004a69bb346165a2
RohanXKapur <rohankapurdev@outlook.com>
2022-06-28 05:11:27 +0800

    add error

--- local/autodca-cli/src/error.rs
+++ local/autodca-cli/src/error.rs
@@ -2,4 +2,6 @@
 pub enum AppError {
     #[error("Network param must be mainnet or devnet")]
     NotValidNetwork,
+    #[error("Token account holds insufficient balance")]
+    InsufficientBalance,
 }
