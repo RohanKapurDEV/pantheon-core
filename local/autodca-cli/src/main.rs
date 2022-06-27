610d76e66a233fc29f233ae4a1228d603f2bbd3c
RohanXKapur <rohankapurdev@outlook.com>
2022-06-28 05:14:04 +0800

    hook up handler to command

--- local/autodca-cli/src/main.rs
+++ local/autodca-cli/src/main.rs
@@ -4,7 +4,6 @@ mod instructions;
 mod utils;
 
 use args::*;
-use error::*;
 use instructions::*;
 use utils::*;
 
@@ -36,7 +35,30 @@ async fn main() {
                     }
                 }
             }
-            _ => {}
+            EntityType::InitDcaMetadata(args) => {
+                let network = args.network;
+                let keypair_path = args.keypair_path;
+
+                let client = build_client(keypair_path.clone(), network);
+                let res = initialize_dca_metadata(
+                    &client,
+                    keypair_path,
+                    args.amount_per_interval,
+                    args.interval_length,
+                    args.max_intervals,
+                    args.crank_authority,
+                    args.from_mint,
+                    args.to_mint,
+                )
+                .await;
+
+                match res {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("{}", e.to_string())
+                    }
+                }
+            }
         },
     }
 }
