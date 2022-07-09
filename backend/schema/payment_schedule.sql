d6cf7c38005ba5bc766cc0ee26f88ce8ea733adb
RohanXKapur <rohankapurdev@outlook.com>
2022-07-10 02:48:54 +0800

    modify schema

--- backend/schema/payment_schedule.sql
+++ backend/schema/payment_schedule.sql
@@ -3,16 +3,5 @@ create table if not exists payment_schedule (
     network varchar(10) not null,
     inserted_at timestamp not null default now(),
     timestamp varchar(50) not null,
-    -- owner_address varchar(50) not null,
-    -- from_token_mint varchar(50) not null,
-    -- to_token_mint varchar(50) not null,
-    -- owner_from_token_account varchar(50) not null,
-    -- owner_to_token_account varchar(50) not null,
-    -- vault_from_token_account varchar(50) not null,
-    -- vault_to_token_account varchar(50) not null,
-    -- amount_per_interval bigint unsigned not null,
-    -- interval_length bigint unsigned not null,
-    -- interval_counter int unsigned not null,
-    -- max_intervals int unsigned not null,
-    -- crank_authority varchar(50) not null
-);
+    dca_metadata_id bigint unsigned not null
+)
