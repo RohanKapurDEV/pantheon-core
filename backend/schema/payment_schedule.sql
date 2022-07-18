7f868f378a735242ef00d96b2f82ccc608c5750b
RohanXKapur <rohankapurdev@outlook.com>
2022-07-19 00:56:29 +0800

    modify schema

--- backend/schema/payment_schedule.sql
+++ backend/schema/payment_schedule.sql
@@ -2,7 +2,7 @@ create table if not exists payment_schedule (
     payment_schedule_id serial primary key,
     network varchar(10) not null,
     inserted_at timestamp not null default now(),
-    timestamp varchar(50) not null,
+    unix_timestamp int unsigned not null,
     dca_metadata_id bigint unsigned not null,
     dca_metadata_address varchar(50) not null
 )
