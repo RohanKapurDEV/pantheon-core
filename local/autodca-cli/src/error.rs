570a35d4168c8bf4e64126e680ed6c2cb3c55ef9
RohanXKapur <rohankapurdev@outlook.com>
2022-07-27 01:43:25 +0800

    allow warning

--- local/autodca-cli/src/error.rs
+++ local/autodca-cli/src/error.rs
@@ -2,6 +2,7 @@
 pub enum AppError {
     #[error("Network param must be mainnet or devnet")]
     NotValidNetwork,
+    #[allow(dead_code)]
     #[error("Token account holds insufficient balance")]
     InsufficientBalance,
 }
